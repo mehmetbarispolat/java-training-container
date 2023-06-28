ce7348d98fbf93eb0283b96cb320b666f5b9f487
Mehmet Baris POLAT <baris.polat@oredata.com>
2023-06-28 17:26:55 +0000

    created spring boot project template

---
+++ src/main/java/dev/mehmetbarispolat/javatrainingcontainer/JavaTrainingContainerApplication.java
@@ -0,0 +1,13 @@
+package dev.mehmetbarispolat.javatrainingcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainingContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mehmetbarispolat/javatrainingcontainer/JavaTrainingContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mehmetbarispolat.javatrainingcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTrainingContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
